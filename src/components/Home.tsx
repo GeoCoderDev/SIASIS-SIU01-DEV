fe67e8419fe7dcca0ee0267f51bcd29beed057a4
Juanito3500 <juanchavezsaldana1@gmail.com>
2024-12-26 23:19:15 -0500

    Se agrego el logo

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,16 @@
-import React from "react";
+import Image from "next/image";
 
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
       <p data-testId="desc">This is my description</p>
+      <Image
+        src="/images/svg/Logo.svg"
+        alt="Next.js Logo"
+        width={200}
+        height={200}
+      />
     </div>
   );
 };
