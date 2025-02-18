ac1eec30dbf42c211f6cef4400f432b6ed6aed14
Juanito3500 <juanchavezsaldana1@gmail.com>
2025-02-18 13:56:17 -0500

    Eliminado archivos innecesarios por el momento

--- src/lib/utils/CustomError.ts
+++
@@ -1,7 +0,0 @@
-export class CustomError extends Error {
-    type: number;
-    constructor(type: number, message: string = "") {
-      super(message);
-      this.type = type;
-    }
-  }
--- src/lib/utils/interfaces/UsedData.ts
+++
@@ -1,7 +0,0 @@
-import { Role } from "@/interfaces/Role";
-
-export interface UserData {
-  username: string;
-  role: Role;
-  urlImage?: string | null;
-}
