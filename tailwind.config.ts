814e1894b761c398366c249236a152f551a516f2
Juanito3500 <juanchavezsaldana1@gmail.com>
2025-02-23 14:47:43 -0500

    Desarrolle la ES2 totalmente responsive!

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -28,6 +28,35 @@ const config: Config = {
         blanco: "var(--blanco)",
         negro: "var(--negro)",
       },
+
+      screens: {
+        sxs: "0px",
+        xs: "300px",
+        sm: "500px",
+        md: "768px",
+        lg: "976px",
+        xl: "1440px",
+
+        "max-xs": { max: "300px" },
+        "max-sm": { max: "500px" },
+        "max-md": { max: "768px" },
+        "max-lg": { max: "976px" },
+        "max-xl": { max: "1440px" },
+
+    
+        "sxs-only": { min: "0px", max: "300px" },
+        "xs-only": { min: "300px", max: "499px" },
+        "sm-only": { min: "500px", max: "767px" },
+        "md-only": { min: "768px", max: "975px" },
+        "lg-only": { min: "976px", max: "1439px" },
+        "xl-only": { min: "1440px" },
+        
+        'short-height': {'raw': '(max-height: 50vw)'}
+
+      },
+
+
+
     },
   },
   plugins: [],
