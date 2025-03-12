1464af92a6e494078e696bf8efac624399e07d36
Juanito3500 <juanchavezsaldana1@gmail.com>
2025-03-12 00:49:17 -0500

    Iniciando seccion de mis datos para directivos 2

---
+++ src/components/shared/cards/UserCard.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const UserCard = () => {
+  return (
+    <div className="[box-shadow:0_0_12px_4px_#00000050] h-min p-[2rem] rounded-[1rem]">
+      <img
+        src="/images/svg/No-Foto-Perfil.svg"
+        alt="Tu foto de Perfil"
+      />
+    </div>
+  );
+};
+
+export default UserCard;
