f1de4a4eff7c7d46a194406cdf2b7f03818b0213
Juanito3500 <juanchavezsaldana1@gmail.com>
2025-03-15 02:44:54 -0500

    Seccion de visualizacion de datos propios para directivos casi terminada 2

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,5 +1,6 @@
 import { getExpirationSessionForRolInSeg } from "@/constants/expirations";
-import { SuccessLoginData } from "@/interfaces/shared/SiasisAPIs";
+import { SuccessLoginData } from "@/interfaces/shared/apis/shared/login/types";
+
 
 import { serialize } from "cookie";
 import { NextRequest } from "next/server";
