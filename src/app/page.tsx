fbb4d2b9f065534fbc392777d51845001f720f72
GuevSus <jesusguevar2003@gmail.com>
2025-03-15 22:13:52 -0500

    Creacion de la pantalla de bienvenida para cada rol

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,13 @@ import { cookies } from "next/headers";
 import React from "react";
 
 const imagenesBienvenida: Record<RolesSistema, string> = {
-  D: "/images/png/imagen.png",
-  PP: "",
-  A: "",
-  PS: "",
-  T: "",
-  R: "",
-  PA: "",
+  D: "images/svg/bienvenida-D.svg",
+  PP: "images/svg/bienvenida-PP-A-PS.svg",
+  A: "images/svg/bienvenida-PP-A-PS.svg",
+  PS: "images/svg/bienvenida-PP-A-PS.svg",
+  T: "images/svg/bienvenida-T.svg",
+  R: "images/svg/bienvenida-R.svg",
+  PA: "images/svg/bienvenida-PA.svg",
 };
 
 const Home = async () => {
@@ -23,22 +23,54 @@ const Home = async () => {
   const genero = cookieStore.get("Genero")!.value as Genero;
 
   return (
-    <div>
-      <h1>¡Hola!</h1>
-      <h3 className="text-gris-oscuro font-semibold">
-        {genero === Genero.Masculino ? "Bienvenido" : "Bienvenida"} de nuevo,{" "}
-        {RolesTextos[rol].desktop[genero]}
-      </h3>
+    <main className="flex flex-col items-center justify-center">
+      {/* Sección de bienvenida */}
+      <div className="flex flex-row items-center justify-around w-full max-w-5xl max-sm:flex-col max-sm:text-left">
+        {/* Texto de bienvenida */}
+        <div className="flex flex-col max-w-[50%] max-sm:max-w-full">
+          <h1 className="text-[5rem] font-bold max-sm:text-[4rem]">¡Hola!</h1>
+          <h3 className="text-gris-oscuro font-semibold text-[1.8rem] max-sm:text-[1.6rem]">
+            {genero === Genero.Masculino ? "Bienvenido" : "Bienvenida"} de nuevo,{" "}
+            {RolesTextos[rol].desktop[genero]}
+          </h3>
+          <h3 className="text-negro text-[1.8rem] max-sm:text-[1.6rem]">
+            {nombres.split(" ").shift()} {apellidos.split(" ").shift()}
+          </h3>
+        </div>
 
-      <h3>
-        {nombres.split(" ").shift()} {apellidos.split(" ").shift()}
-      </h3>
+        {/* Imagen de bienvenida */}
+        <div className="flex justify-center items-center max-sm:mt-6">
+          <img
+            src={imagenesBienvenida[rol]}
+            alt="Imagen de Bienvenida"
+            className="w-[17rem] h-[14rem] max-sm:w-[14rem] max-sm:h-[14rem]"
+          />
+        </div>
+      </div>
 
-      <img
-        src={imagenesBienvenida[rol]}
-        alt="Imagen de Bienvenida al Sistema"
-      />
-    </div>
+      {/* Sección de introducción */}
+      <div className="flex flex-row items-center justify-around w-full max-w-5xl mt-10 max-sm:flex-col max-sm:items-center max-sm:text-left">
+              {/* Contenedor del título y texto */}
+              <div className="flex flex-col max-w-[60%] max-sm:max-w-full text-left max-sm:text-left">
+                {/* Título SIASIS */}
+                <p className="text-[2rem] font-extrabold text-color-interfaz max-sm:text-[1.6rem]">
+                  S I A S I S
+                </p>
+                
+                {/* Texto de introducción */}
+                <p className="text-[1rem] text-negro max-sm:text-[1.1rem] mt-2">
+                ¡Nos emociona presentar esta nueva y avanzada sistema de asistencia! Diseñada por estudiantes de <strong>Ingeniería de Sistemas de la Universidad Nacional de Cañete</strong> con pasión y dedicación. ¡Bienvenidos al futuro de la educación!
+                </p>
+              </div>
+
+              {/* Imagen de introducción */}
+              <img
+                src="images/svg/logo-undc.svg"
+                alt="Logo UNDC"
+                className="w-[15rem] h-[11rem] max-sm:w-[15rem] max-sm:h-[11rem] max-sm:mt-6"
+              />
+      </div>
+    </main>
   );
 };
 
