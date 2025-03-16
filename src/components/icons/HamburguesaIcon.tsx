4f69b553fe057cddb4f1eab08ccdee548e1850a8
Juanito3500 <juanchavezsaldana1@gmail.com>
2025-03-15 22:35:40 -0500

    visualizacion de datos de directivos practicamente completada 3

--- src/components/icons/HamburguesaIcon.tsx
+++ src/components/icons/HamburguesaIcon.tsx
@@ -1,6 +1,6 @@
 import { IconProps } from "@/interfaces/IconProps";
 
-const HamburguesaIcon = ({ className, color, title }: IconProps) => {
+const HamburguesaIcon = ({ className, title }: IconProps) => {
   return (
     <div title={title}>
       <svg
@@ -9,15 +9,15 @@ const HamburguesaIcon = ({ className, color, title }: IconProps) => {
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <rect width="44" height="6" rx="3" fill={color ?? "white"} />
-        <rect width="44" height="6" rx="3" fill={color ?? "white"} />
-        <rect width="44" height="6" rx="3" fill={color ?? "white"} />
-        <rect y="14" width="44" height="6" rx="3" fill={color ?? "white"} />
-        <rect y="14" width="44" height="6" rx="3" fill={color ?? "white"} />
-        <rect y="14" width="44" height="6" rx="3" fill={color ?? "white"} />
-        <rect y="28" width="44" height="6" rx="3" fill={color ?? "white"} />
-        <rect y="28" width="44" height="6" rx="3" fill={color ?? "white"} />
-        <rect y="28" width="44" height="6" rx="3" fill={color ?? "white"} />
+        <rect width="44" height="6" rx="3" className="fill-current" />
+        <rect width="44" height="6" rx="3" className="fill-current" />
+        <rect width="44" height="6" rx="3" className="fill-current" />
+        <rect y="14" width="44" height="6" rx="3" className="fill-current" />
+        <rect y="14" width="44" height="6" rx="3" className="fill-current" />
+        <rect y="14" width="44" height="6" rx="3" className="fill-current" />
+        <rect y="28" width="44" height="6" rx="3" className="fill-current" />
+        <rect y="28" width="44" height="6" rx="3" className="fill-current" />
+        <rect y="28" width="44" height="6" rx="3" className="fill-current" />
       </svg>
     </div>
   );
