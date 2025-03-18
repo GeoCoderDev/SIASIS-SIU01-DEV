72587f7068fa82bef03d07d4473d280251b10067
Juanito3500 <juanchavezsaldana1@gmail.com>
2025-03-18 14:18:41 -0500

    Correcion en la instanciacion de los componentes de Loaders

--- src/components/shared/loaders/Loader.tsx
+++ src/components/shared/loaders/Loader.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Loader = ({className}:{className?:string}) => {
   return (
     <>
-      <div  className={`loader  bg-white ${className}`}></div>
+      <div  className={`loader ${className}`}></div>
     </>
   );
 };
