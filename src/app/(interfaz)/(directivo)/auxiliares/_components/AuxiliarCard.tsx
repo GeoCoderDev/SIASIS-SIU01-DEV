dd2df21a4feca60ce184592bad487aa6a1a2ea92
AndersonA <arataacevedo34@gmail.com>
2025-04-08 17:41:07 -0600

    Actualización-de-la-interfaz-directiva-auxiliarV2

--- src/app/(interfaz)/(directivo)/auxiliares/_components/AuxiliarCard.tsx
+++ src/app/(interfaz)/(directivo)/auxiliares/_components/AuxiliarCard.tsx
@@ -20,39 +20,38 @@ const AuxiliarCard = ({
   Auxiliar: AuxiliarSinContraseña;
 }) => {
   return (
-    <div className="w-[311px] h-[367.65px] flex flex-col items-center gap-2 p-6 rounded-[10px] shadow-[0_0_3px_6px_#00000050]">
+    <div className="w-[311px] h-[385px] rounded-[15px] shadow-[0_0_6px_3px_rgba(0,0,0,0.15)] flex flex-col items-center justify-start p-4 gap-2 text-center bg-white">
       <FotoPerfilClientSide
-        className="w-[75px] h-[75px] rounded-full object-cover"
+        className="w-[85px] h-[85px] rounded-full object-cover"
         Google_Drive_Foto_ID={Google_Drive_Foto_ID}
       />
-      <span className="text-[20px] font-semibold text-center">
+
+      <span className="text-[20px] font-semibold text-negro">
         {Nombres} {Apellidos}
       </span>
-      <span className="text-[20px] text-azul-principal font-medium">
+
+      <span className="text-[20px] font-medium text-azul-principal">
         {DNI_Auxiliar}
       </span>
-      <span className="italic font-semibold text-[16px]">
-        {Nombre_Usuario}
-      </span>
-      <span className="flex items-center gap-1 text-[16px]">
-        <TelefonoIcon className="w-[1.2rem] text-verde-principal" /> {Celular}
-      </span>
-      <span className="text-[13.29px] font-semibold text-center">
-        {Correo_Electronico}
-      </span>
-      <span
-        className={`text-[16px] font-semibold ${
-          Estado ? "text-verde-principal" : "text-rojo-oscuro"
-        }`}
-      >
+
+      <span className="italic text-[16px] text-negro font-bold">{Nombre_Usuario}</span>
+
+      <div className="flex items-center justify-center gap-1 text-[16px] text-negro">
+        <TelefonoIcon className="w-[1.2rem] text-verde-principal" />
+        <span>{Celular}</span>
+      </div>
+
+      <span className="text-[13.29px] text-negro font-bold">{Correo_Electronico}</span>
+
+      <span className="text-[16px] font-semibold text-verde-principal">
         Estado: {Estado ? "Activo" : "Inactivo"}
       </span>
 
-      <Link href={`/auxiliares/${DNI_Auxiliar}`} className="mt-auto">
+      <Link href={`/auxiliares/${DNI_Auxiliar}`} className="mt-2">
         <BotonConIcono
-          className="bg-amarillo-ediciones flex gap-2 items-center px-3 py-2"
+          className="bg-amarillo-ediciones text-negro font-medium flex gap-2 items-center px-3 py-2 rounded"
           texto="Ver/Editar"
-          IconTSX={<VerEditarIcon className="w-5" />}
+          IconTSX={<VerEditarIcon className="w-4 h-4" />}
         />
       </Link>
     </div>
