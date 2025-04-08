bc80f98add68f4d201cbca4737b8511a923a2b91
AndersonA <arataacevedo34@gmail.com>
2025-04-08 17:22:04 -0600

    Actualización-de-la-interfaz-directiva-auxiliar

--- src/app/(interfaz)/(directivo)/auxiliares/_components/AuxiliarCard.tsx
+++ src/app/(interfaz)/(directivo)/auxiliares/_components/AuxiliarCard.tsx
@@ -20,31 +20,37 @@ const AuxiliarCard = ({
   Auxiliar: AuxiliarSinContraseña;
 }) => {
   return (
-    <div className="flex flex-col gap-2.5 p-6 rounded-[10px] shadow-[0_0_3px_6px_#00000050]">
+    <div className="w-[311px] h-[367.65px] flex flex-col items-center gap-2 p-6 rounded-[10px] shadow-[0_0_3px_6px_#00000050]">
       <FotoPerfilClientSide
-        className="w-[2rem]"
+        className="w-[75px] h-[75px] rounded-full object-cover"
         Google_Drive_Foto_ID={Google_Drive_Foto_ID}
       />
-      <span>
+      <span className="text-[20px] font-semibold text-center">
         {Nombres} {Apellidos}
       </span>
-      <span className="text-azul-principal font-semibold"> {DNI_Auxiliar}</span>
-      <span className="font-semibold">{Nombre_Usuario}</span>
-      <span>
+      <span className="text-[20px] text-azul-principal font-medium">
+        {DNI_Auxiliar}
+      </span>
+      <span className="italic font-semibold text-[16px]">
+        {Nombre_Usuario}
+      </span>
+      <span className="flex items-center gap-1 text-[16px]">
         <TelefonoIcon className="w-[1.2rem] text-verde-principal" /> {Celular}
       </span>
-      <span className="font-semibold">{Correo_Electronico}</span>
+      <span className="text-[13.29px] font-semibold text-center">
+        {Correo_Electronico}
+      </span>
       <span
-        className={`font-semibold ${
+        className={`text-[16px] font-semibold ${
           Estado ? "text-verde-principal" : "text-rojo-oscuro"
         }`}
       >
         Estado: {Estado ? "Activo" : "Inactivo"}
       </span>
 
-      <Link href={`/auxiliares/${DNI_Auxiliar}`}>
+      <Link href={`/auxiliares/${DNI_Auxiliar}`} className="mt-auto">
         <BotonConIcono
-          className="bg-amarillo-ediciones flex gap-2 items-center px-3 py-2 "
+          className="bg-amarillo-ediciones flex gap-2 items-center px-3 py-2"
           texto="Ver/Editar"
           IconTSX={<VerEditarIcon className="w-5" />}
         />
